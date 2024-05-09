f8888df19742adf295bcc22a99e63e6e9d3c42c0
Ajinkya Rajandekar <ajinkya.raj@trili.tech>
2024-05-09 12:08:35 +0100

    Use chakra layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,15 +9,18 @@ export const metadata: Metadata = {
   title: 'Staking Tez',
   description: 'Developed by Trilitech Ltd.'
 }
+// app/layout.tsx
 
 export default function RootLayout({
   children
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
