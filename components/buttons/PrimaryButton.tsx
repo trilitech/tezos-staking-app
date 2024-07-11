3d48364a423d795975ea074880cb5ae13cca8117
rickykuo666 <ricky.kuo@trili.tech>
2024-07-11 08:47:49 +0100

    remove max width for buttons

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -14,7 +14,6 @@ export const PrimaryButton = ({
   return (
     <Button
       w='100%'
-      maxW='400px'
       bg={disabled ? '#E2E8F0' : '#0052FF'}
       color={disabled ? '#A0AEC0' : 'white'}
       fontWeight={600}
