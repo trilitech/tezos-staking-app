f7bdef0688a5d5d29fd7b25ab79c2bbb5285d551
rickykuo666 <ricky.kuo@trili.tech>
2025-06-03 16:49:03 +0100

    types

--- pages/global.d.ts
+++ pages/global.d.ts
@@ -1,6 +1,6 @@
 import { RecipeComponentProps } from '@chakra-ui/react'
 // Use a **relative** import to your recipe file:
-import { buttonRecipe } from './theme/recipes/button.recipe'
+import { buttonRecipe } from '../theme/recipes/button.recipe'
 
 // Now augment the ButtonProps interface so it includes `visual` (and any other recipe props)
 declare module '@chakra-ui/react' {
