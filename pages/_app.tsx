ee818e33c8f5d5ac2fd624d345b99f6b1e82192b
rickykuo666 <ricky.kuo@trili.tech>
2025-09-10 14:15:21 +0100

    use posthog provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,35 @@ import { Provider } from '@/components/ui/provider'
 import { ConnectionProvider } from '@/providers/ConnectionProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { OperationResponseProvider } from '@/providers/OperationResponseProvider'
+import { useEffect } from 'react'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      defaults: '2025-05-24',
+      loaded: posthog => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      }
+    })
+  }, [])
+
   return (
-    <OperationResponseProvider>
-      <QueryClientProvider client={queryClient}>
-        <ConnectionProvider>
-          <Provider>
-            <Component {...pageProps} />
-          </Provider>
-        </ConnectionProvider>
-      </QueryClientProvider>
-    </OperationResponseProvider>
+    <PostHogProvider client={posthog}>
+      <OperationResponseProvider>
+        <QueryClientProvider client={queryClient}>
+          <ConnectionProvider>
+            <Provider>
+              <Component {...pageProps} />
+            </Provider>
+          </ConnectionProvider>
+        </QueryClientProvider>
+      </OperationResponseProvider>
+    </PostHogProvider>
   )
 }
